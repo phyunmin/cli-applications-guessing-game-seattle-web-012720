b02bd8ed86c902d35000c03ac559490e91d4d708
Hyunmin Park <hyunmin.park.career@gmail.com>
2020-01-09 21:50:03 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+def run_guessing_game
+  rannum = rand(5)+1
+  puts "Choose the number between 1 and 6"
+  input = gets.chomp
+  
+  if input == rannum.to_s
+    puts "You guessed the correct number!"
+  elsif input =='exit'
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{rannum}."
+  end
+  
+end
