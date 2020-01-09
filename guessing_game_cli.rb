bf14a3d877f51b7ab130125337b33f85659e53d2
Hyunmin Park <hyunmin.park.career@gmail.com>
2020-01-09 21:47:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Choose the number between 1 and 6"
   input = gets.chomp
   
-  if rannum == input
+  if input == rannum
     puts "You guessed the correct number!"
   elsif input =='exit'
     puts "Goodbye!"
